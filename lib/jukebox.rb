9619a6bbe874ac1dfdfe45c6382289c9456fd22e
Sanny Do <sanny.do94@gmail.com>
2020-09-26 01:37:49 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -102,6 +102,8 @@ end
 # call the `help` method and if their input is `"exit"`, call the `exit_jukebox`
 # method _and_ break out of your loop to stop the program.
 
+
+
 # def run(songs)
 #   help
 #   puts "Please enter a command: "
@@ -121,3 +123,30 @@ end
 #       end
 #     exit_jukebox
 # end
+
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  user_response = gets.chomp
+  while user_response != "exit"
+    case user_response
+      when "help"
+        help
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      when "list"
+        list(songs)
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      when "play"
+        play(songs)
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      else
+        puts "Invalid input, please try again"
+        user_response = gets.chomp
+    end
+  end
+  exit_jukebox
+end 
