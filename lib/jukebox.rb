6f3b3ab97d8c0d36fa814dcac485cd2bc77a8a88
Sanny Do <sanny.do94@gmail.com>
2020-09-26 00:33:20 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,7 +31,7 @@ puts "- help : displays this help message"
 puts "- list : displays a list of songs you can play"
 puts "- play : lets you choose a song to play"
 puts "- exit : exits this program"
-
+end
 
 #same thing as list below this method, making sure the index starts at 1
 # def list(songs) 
